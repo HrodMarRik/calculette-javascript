cab69d22e2d9382f78f8f8407e9d2a7b9d6bdb0a
HrodMarRik <somosminal@gmail.com>
2024-06-07 08:35:12 +0200

    auto-up

--- main.js
+++ main.js
@@ -4,16 +4,55 @@ function calculate(event) {
     var taux_nominal = document.getElementsByName('taux_nominal')[0].value;
     var duree_remboursement = document.getElementsByName('duree_remboursement')[0].value;
     var printer = document.querySelector('.printer');
-    var error = document.getElementByID('error')
+    var error_printer = document.getElementById('error_printer');
+    var error = ""
     if (montant_emprunter == "" || taux_nominal == "" || duree_remboursement == "") {
-        printer.innerHTML = "vide";
-        console.log("vide");
+        printer.innerHTML =""
+        document.getElementById('error_printer').style.display = 'block';
     } else {
+        var regex_entier_decimal = /^\d+(\.\d+)?$/;
+        var regex_decimal = /^\d+(\.\d+)$/;
+        var regex_entier = /^\d+$/;
+        
+        document.getElementById('error_printer').style.display = 'none';
+        
+        if (!regex_entier_decimal.test(montant_emprunter)) {
+            error += "Le montant emprunté doit être un nombre entier ou decimal.";
+        }
+        
+        if (!regex_decimal.test(taux_nominal)) {
+            error += "Le taux nominal doit être un decimal.";
+        }
+        
+        if (!regex_entier.test(duree_remboursement)) {
+            error += "La durée de remboursement doit être un nombre entier.";
+        }
+        
+        if (error != "") {
+            error_printer.innerHTML = error;
+            document.getElementById('error_printer').style.display = 'block';
+            return;
+        }
 
-        printer.innerHTML = "<p>Résultat du calcul : " + montant_emprunter + "</p>";
-        console.log("validé");
+        printer.innerHTML = html(montant_emprunter, taux_nominal, duree_remboursement);
     }
 }
-function html(argument) {
-    // body...
+
+function html(K, T, n) {
+    var html = "";
+    var nombre_mois = duree_remboursement * 12;
+    var M = (K * taux_periodique) / (1 - Math.pow((1 + taux_periodique), -n));
+
+    // Calcul du coût total du crédit
+    var cout_total = M * n - K;
+
+    // Calcul des intérêts totaux payés
+    var interets_totaux = cout_total - K;
+
+    // Génération du HTML avec les résultats
+    html += "<p>Montant des mensualités : " + M.toFixed(2) + " €</p>";
+    html += "<p>Coût total du crédit : " + cout_total.toFixed(2) + " €</p>";
+    html += "<p>Intérêts totaux payés : " + interets_totaux.toFixed(2) + " €</p>";
+
+    return html;
 }
